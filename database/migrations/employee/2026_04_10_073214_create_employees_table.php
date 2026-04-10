d1c7e45d09de7a3716e6504ac6908a5b278e01b7
Nandhakumar <nandhakumararunachalam2003@gmail.com>
2026-04-10 15:49:43 +0530

    migration path changes

--- database/migrations/employee/2026_04_10_073214_create_employees_table.php
+++ database/migrations/employee/2026_04_10_073214_create_employees_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,26 +12,24 @@ return new class extends Migration
      */
     public function up()
     {
-        public function up(): void
-{
-    Schema::create('employees', function (Blueprint $table) {
-        $table->id();
-        $table->string('employee_code')->unique();
-        $table->string('full_name');
-        $table->date('dob')->nullable();
-        $table->enum('gender', ['M', 'F', 'O'])->nullable();
-        $table->integer('phone')->nullable();
-        $table->string('email')->nullable();
-        $table->date('joining_date')->nullable();
-        $table->unsignedBigInteger('department_id')->nullable();
-        $table->string('status')->nullable();
-        $table->unsignedBigInteger('reporting_manager_id')->nullable();
-        $table->date('probation_end_date')->nullable();
-        $table->unsignedBigInteger('designation_id');
-        $table->timestamps();
-    });
-}
+
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('employee_code')->unique();
+            $table->string('full_name');
+            $table->date('dob')->nullable();
+            $table->enum('gender', ['M', 'F', 'O'])->nullable();
+            $table->integer('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->date('joining_date')->nullable();
+            $table->unsignedBigInteger('department_id')->nullable();
+            $table->string('status')->nullable();
+            $table->unsignedBigInteger('reporting_manager_id')->nullable();
+            $table->date('probation_end_date')->nullable();
+            $table->unsignedBigInteger('designation_id');
+            $table->timestamps();
         });
+
     }
 
     /**
--- database/migrations/employee/2026_04_10_084638_create_designations_table.php
+++ database/migrations/employee/2026_04_10_084638_create_designations_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,12 +12,13 @@ return new class extends Migration
      */
     public function up()
     {
-        $table->id();
-        $table->string('title');
-        $table->string('grade')->nullable();
-        $table->integer('level')->nullable();
-        $table->boolean('is_managerial')->default(false);
-        $table->timestamps();
+        Schema::create('designations', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('grade')->nullable();
+            $table->integer('level')->nullable();
+            $table->boolean('is_managerial')->default(false);
+            $table->timestamps();
         });
     }
 
